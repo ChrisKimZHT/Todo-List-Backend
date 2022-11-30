75ba6655d5f6751b49e76e8dbd3619359208241f
ChrisKim <1515748548@qq.com>
2022-11-30 21:48:16 +0800

    [fix]: 解决跨域问题

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from api.todo import todo_bp
 from api.note import note_bp
+from flask_cors import *
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 
 @app.route('/')
