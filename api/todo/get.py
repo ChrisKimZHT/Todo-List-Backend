a341166553116a735fa857ac25d28ac16cc06ecf
ChrisKim <1515748548@qq.com>
2022-12-01 10:10:44 +0800

    [feat]: /todo/get 完成

--- api/todo/get.py
+++ api/todo/get.py
@@ -1,6 +1,53 @@
 from api.todo import todo_bp
+from flask import request, abort, jsonify
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 @todo_bp.route("/get", methods=["GET"])
 def todoGet():
-    return "/todo/get"
+    request_arg = request.args.to_dict()
+
+    # 数据校验
+    try:
+        request_id = int(request_arg["id"])
+    except ValueError as e:
+        abort(400, description=f"Request Format Error. {e}")
+        return
+
+    # 连接数据库
+    try:
+        mydb = mysql.connector.connect(
+            host=os.environ.get("db_host"),
+            user=os.environ.get("db_user"),
+            password=os.environ.get("db_password"),
+            database=os.environ.get("db_name")
+        )
+    except Exception as e:
+        abort(500, description=f"Database Connection Error. {e}")
+        return
+
+    # 数据库操作
+    try:
+        mycursor = mydb.cursor()
+        sql = f"SELECT * FROM todo WHERE id={request_id}"
+        mycursor.execute(sql)
+        data = mycursor.fetchall()[0]
+        selected_todo = {
+            "id": data[0],
+            "title": data[1],
+            "detail": data[2],
+            "begin": data[3],
+            "end": data[4],
+            "isDeadLine": bool(data[5]),
+            "isFinished": bool(data[6]),
+        }
+        return jsonify({"data": selected_todo, "status": 0, "message": "OK"})
+    except Exception as e:
+        abort(500, description=f"Database Operation Error. {e}")
+        return
+    finally:
+        mydb.close()
